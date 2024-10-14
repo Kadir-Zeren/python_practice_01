c35069b6044386c552497e13e36e0f989579e8f1
kadir-zeren <k4dzeren@gmail.com>
2024-10-14 10:07:13 +0300

    practice

--- python.py
+++ python.py
@@ -1,1 +1,72 @@
-print ("hello")
+print("hello")
+print("Hello world")
+print('Benim yazdigim ilk kod')
+print('Hello World!')
+print("ben bunu yazmak istiyorum")
+print('Hello World!')
+print("Hello World!")
+print ( 'Hello World! ')
+print("Clarusway School!")
+print("I'm happy to learn!")
+print("Cift tirnak yazdim")
+print('''uc tirnak ile yazdim''')
+print("bir cumle")
+print("I'm great")
+print('"hi" he said')
+print ('''"Don't do it" said the manager''')
+print ( 'Hello World! ')
+print("Clarusway School!")
+print("I'm happy to learn!")
+print('''Lorem ipsum dolor sit amet,
+fusce ut quisque neque donec,
+massa metus amet, luctus inceptos. ''')
+print("""Praesent a, mi mattis velit metus,
+accumsan adipiscing ipsum sit justo
+penatibus, amet mauris non tempus justo. """)
+print('ben bunu yazmak istiyorum')
+print('''bu birinci satir
+bu ikinci satir
+bu uc
+bu dort''')
+print('3.14')
+print(3.14)
+print('3.14'*3)
+print(3.14*3)
+print(''''We should have enough time for our family"''')
+print('first line' )
+print ()
+print('''third line''')
+print('bir')
+print ()
+print('uc')
+print()
+print('')
+print('I\'m great' )
+print("\"don't do it\" said the manager")
+print('bu benim cumlem \nbu ikinci satir')
+print('bu benim cumlem \tbu ikinci satir')
+print('being a good person')
+print("ben,kodlarkjlj vl k k k kk 11 1kjlkjlkjljlk \
+kdkdkdkdkdkd\
+kdkdkdkdkd")
+# buradaki fonksiyon kare alma fonksiyonu
+print('kullanici ismi') # Burada kullanici ismi yazddiriliyor.
+x = 5# burada x degeri atandi
+X = 10
+y = 5
+a = (x / y) * 25
+print(a ** 2)
+
+x = 10
+y = 5
+a = (x / y) * 100
+print(a)
+
+# This is a single line comment
+
+print('hello') # This is an inline .comment
+
+print ( 'hello')
+# First multi-line comment
+# Second multi-line comment
+# Third multi-line comment
